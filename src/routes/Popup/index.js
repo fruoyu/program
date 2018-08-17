bb92d3466058a336ddb6fd0fb9656e8e0bd13933
yexiaoqi <yexq@polysoft.com.cn>
2018-08-17 09:37:56 +0800

    添加音频播放器

--- src/routes/Popup/index.js
+++ src/routes/Popup/index.js
@@ -9,7 +9,7 @@ import $ from 'jquery';
 // import '../../assets/css/public.css';
 import './popup.less';
 import '../../assets/iconfont/iconfont.css';
-import '../../plugs/audio/audio.js';
+// import '../../plugs/audio/audio.js';
 import '../../plugs/audio/audio.css';
 
 class Popup extends Component {
@@ -179,7 +179,7 @@ class Popup extends Component {
         },
       ],
       customer: {
-        CUSTOMER_CHILD:'子女信息',
+        CUSTOMER_CHILD: '子女信息',
         CUSTOMER_AGE:'年龄',
         CUSTOMER_SEX:'性别',
         CUSTOMER_MERRIGE:'婚姻状况',
@@ -234,11 +234,17 @@ class Popup extends Component {
       },
       clickIndex: 0,
       hoverIndex: -1,
+      isPlay: false,
     };
   }
   componentDidMount() {
     if (this.props.location.pathname === '/') {
       this.props.dispatch(routerRedux.push('/login'));
+    }
+    if (this.state.isPlay) {
+      $('#audio-content')[0].pause();
+    } else {
+      $('#audio-content')[0].play();
     }
   }
 
@@ -292,9 +298,45 @@ class Popup extends Component {
             </div>
             <div className="modal-content">
               <div className="archivesAudio">
-                <div id="audioEle"></div>
+                <div id="audioEle">
+                  <div className="wx-audio-content" style={{ width: '100%' }}>
+                    <audio className="wx-audio-content" src={{uri: "http://47.95.113.97:8660/file/20180810211904920战旗-王丽-苗女士-13611323882.mp3"}} ref='audioContent' id='audio-content'></audio>
+                    <div className="wx-audio-right">
+                      <p className="middleX"></p>
+                      <div className="wx-audio-time">
+                        <span className="current-t">00:00</span>
+                        <span className="duration-t">33:44</span>
+                      </div>
+                      <div className="wx-audio-progrees">
+                        <div className="wx-progrees-detail">
+                          <span className="wx-voice-p"></span>
+                          <span className="wx-buffer-p"></span>
+                          <span className="wx-loading">
+                            <span className="wx-loading-wrapper"></span>
+                          </span>
+                        </div>
+                        <div className="wx-audio-origin"></div>
+                      </div>
+                    </div>
+                    <div className="wx-audio-left">
+                      <i className={['iconfont', this.state.isPlay ? "icon-zanting" : "icon-bofang"].join(' ')} onClick={() => {
+                        if (this.state.isPlay) {
+                          console.log('暂停');
+                          this.setState({
+                            isPlay: false,
+                          })
+                        } else {
+                          console.log('播放');
+                          this.setState({
+                            isPlay: true,
+                          })
+                        }
+                      }}></i>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="insightTextWrap" style={{ boxSizing: 'border-box'}}>
+              <div className="insightTextWrap" style={{ boxSizing: 'border-box' }}>
               </div>
               <Scrollbars>
                 <div className="insightTermWrap">
@@ -365,27 +407,22 @@ class Popup extends Component {
               <ul id="file-list">
                 {
                   this.state.fileList.map((item, index) => (
-                    <li 
-                      className={["file-item", index == this.state.clickIndex ? 'item-active-2' : '', index == this.state.hoverIndex ? 'item-active' : ''].join(' ')} 
-                      data-name={item.id} 
-                      data-status={item.statusMessage} 
-                      key={index}
+                    <li className={['file-item', index == this.state.clickIndex ? 'item-active-2' : '', index == this.state.hoverIndex ? 'item-active' : ''].join(' ')} data-name={item.id} data-status={item.statusMessage} key={index}
                       onClick={() => {
                         this.setState({
-                          clickIndex: index
-                        })
+                          clickIndex: index,
+                        });
                       }}
                       onMouseEnter={() => {
                         this.setState({
-                          hoverIndex: index
-                        })
+                          hoverIndex: index,
+                        });
                       }}
                       onMouseLeave={() => {
                         this.setState({
-                          hoverIndex: -1
-                        })
-                      }}
-                      >
+                          hoverIndex: -1,
+                        });
+                      }}>
                       <span className="item-name">{item.fileName}</span>
                       <span className="item-size">{item.size}</span>
                     </li>
